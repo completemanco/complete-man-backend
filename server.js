8c8ff8222cb21dcfee702bef47f8984066012914
completemanco <travisholton8@gmail.com>
2026-09-20 11:29:58 -0400

    server.js

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ import Stripe from "stripe";
 const { Pool } = pg;
 const app = express();
 app.use(cors());
-app.use(express.json());
+app.use(express.json({ limit: "12mb" })); // room for course file uploads
 
 const DAILY_API_KEY = process.env.DAILY_API_KEY;
 const DAILY_API = "https://api.daily.co/v1";
